e7d7e9a25d3d02f789d26663545a768dd13f1f50
Jil Sejpal <jilsejpal7@gmail.com>
2023-04-29 12:08:19 +0530

    commit code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import MapScreen from './screens/MapScreen';
 import Draggable from './screens/Draggable';
-import AddText from './screens/AddText';
-import DrawingCanvas from './screens/Drawing';
 import Drawing from './screens/Drawing';
 
 const App = () => {
